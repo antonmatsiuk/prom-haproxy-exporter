81288ea206242b495864e9eba1da4515f879fa40
Anton Matsiuk <ama@facelift-bbt.com>
2019-01-25 16:16:21 +0100

    add molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_supervisord_running_and_enabled(host):
+    haproxy_exporter = host.service('supervisord')
+    assert haproxy_exporter.is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_haproxy_exporter_running(host):
+    ansible_vars = \
+        host.ansible("include_vars",
+                     "file=../../defaults/main.yml")["ansible_facts"]
+    haproxy_exporter_port = ansible_vars["haproxy_exporter_port"]
+    haproxy_exporter_ip = ansible_vars["haproxy_exporter_ip"]
+    socket = host.socket('tcp://%s:%s' % (haproxy_exporter_ip,
+                                          haproxy_exporter_port))
+    assert socket.is_listening
